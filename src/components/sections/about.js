73940debef50293ac9e4161a7e6656b1a7c86222
rajdigitech <18703030+Rajpolu@users.noreply.github.com>
2021-02-27 17:40:57 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -141,7 +141,7 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>Hello! I'm Brittany, a software engineer based in Boston, MA.</p>
+            <p>Hello! I'm Rajasekhar Reddy Polu, a software engineer based in Chennai, IN.</p>
 
             <p>
               I enjoy creating things that live on the internet, whether that be websites,
@@ -152,7 +152,7 @@ const About = () => {
             <p>
               Shortly after graduating from{' '}
               <a href="https://www.ccis.northeastern.edu">Northeastern University</a>, I joined the
-              engineering team at <a href="https://www.upstatement.com">Upstatement</a> where I work
+              engineering team at <a href="https://www.Google.com">Google India</a> where I work
               on a wide variety of interesting and meaningful projects on a daily basis.
             </p>
 
