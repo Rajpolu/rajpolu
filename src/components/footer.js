4a7ae44ee24736142b5b54e77358cc240d573335
rajdigitech <18703030+Rajpolu@users.noreply.github.com>
2021-02-27 18:24:51 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/rajpolu/">
           <div>Designed &amp; Built by RajasekharReddy Polu</div>
         </a>
       </StyledCredit>
