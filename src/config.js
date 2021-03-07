5f5c74f9ad552932f4c5f21eb3418b480ad67d8b
rajdigitech <18703030+Rajpolu@users.noreply.github.com>
2021-03-07 14:23:13 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,23 +4,23 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/rajpolu',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/rajasekharreddypolu',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/@rockyprreddy',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/rajasekharreddypolu',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/rajpolu',
     },
   ],
 
